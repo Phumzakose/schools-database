eaa207f35105269fc01b3a8b005c1fb8321741d3
Phumza kose <kosephumza@gmail.com>
2022-10-18 10:30:35 +0200

    modified functions

--- sql/functions/add_teacher.sql
+++ sql/functions/add_teacher.sql
@@ -1,26 +1,25 @@
 create or replace function 
-	add_teacher(the_last_name text )
-	returns boolean as
+	add_teacher ( the_first_name text, the_last_name text, the_email text )
+	returns text as
 $$
 declare
 
 total int;
 
 begin
-
-
-	select into total count(*) from teachers 
-		where LOWER(last_name) = LOWER(the_first_name);
+	
+	select into total count(*) from teacher 
+		where LOWER(email)= LOWER(the_email);
 
 
 	if (total = 0) then
-	
-		insert into teacher(last_name) values (the_last_name);
-	
-		return true;
+    	insert into teacher (first_name, last_name, email) values (the_first_name, the_last_name, the_email);
+
+			return 'Teacher is added in the database' ;
+ 
 	else
-		
-		return false;
+	
+		return 'Teacher is already in the database';
 	end if;
 
 end;
